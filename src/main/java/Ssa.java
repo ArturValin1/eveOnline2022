1e43b8e0abff993cbe6440c2088e0320adb4945c
Artur Valin <artur.valin@yandex.ru>
2022-07-09 20:31:24 +0900

    1.3-3-01

--- src/main/java/Ssa.java
+++ src/main/java/Ssa.java
@@ -8,13 +8,21 @@ public class Ssa {
     public static void main(String[] args) {
         BlackAndWhite bw = new BlackAndWhite();
         try {
-            BufferedImage sub = ImageIO.read(new File("src/main/resources/images/R1980x1080/kmBW.png"));
+//            BufferedImage sub = ImageIO.read(new File("src/main/resources/images/R1980x1080/kmBW.png"));
+            BufferedImage sub = ImageIO.read(new File("src/main/resources/images/R1980x1080/bw/bw0.png"));
             BufferedImage image = ImageIO.read(new File("src/main/resources/images/R1980x1080/test01BW.png"));
             BufferedImage sub1 = ImageIO.read(new File("src/main/resources/images/R1980x1080/m3BW.png"));
-            Point p =bw.findSubImageInBigImage(sub, image);
+            Point p = bw.findSubImageInBigImage(sub, image);
+            if (p != null)
+                System.out.println(p.x + " " + p.y);
+            else {
+                bw.printImage(sub);
+                bw.printImage(image);
+                System.out.println("Not found");
+            }
+            p = bw.findSubImageInBigImage(sub1, image);
             System.out.println(p.x + " " + p.y);
-            p =bw.findSubImageInBigImage(sub1, image);
-            System.out.println(p.x + " " + p.y);
+            bw.printImage(sub1);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
